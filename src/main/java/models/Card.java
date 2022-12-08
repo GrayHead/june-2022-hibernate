1d616a074915c69ee5e89f65193492619dd784ef
GrayHead <ss.zhuravlov@gmail.com>
2022-12-08 21:16:38 +0200

    lesson 2.5 otm bd

--- src/main/java/models/Card.java
+++ src/main/java/models/Card.java
@@ -19,6 +19,14 @@ public class Card {
     private int id;
     private String number;
 
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_card",
+            joinColumns = @JoinColumn(name = "card_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private User user;
+
     public Card(String number) {
         this.number = number;
     }
